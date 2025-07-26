60c487ac08f8aec533e5e22ed2fb77cbb5519d66
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2025-07-26 10:01:32 +0200

    Delete pietnasty_lipiec25 directory

--- pietnasty_lipiec25/src/main/java/com/example/pietnasty_lipiec25/user/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.pietnasty_lipiec25.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
