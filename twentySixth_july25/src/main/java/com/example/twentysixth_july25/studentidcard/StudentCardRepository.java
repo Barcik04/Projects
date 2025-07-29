abd235766b1a2bc679b754cc2284691df9466aee
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2025-07-29 20:21:13 +0200

    Delete twentySixth_july25 directory

--- twentySixth_july25/src/main/java/com/example/twentysixth_july25/studentidcard/StudentCardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.twentysixth_july25.studentidcard;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentCardRepository extends JpaRepository<StudentIdCard, Long> {
-}
